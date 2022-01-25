477514576161aa85504e803c80573bc8854a83ec
Chong <monkey.d.chong@gmail.com>
2022-01-25 18:50:38 +0700

    init

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+      <div class="inner">
+        <div class="my-5">
+          <a href="smartsales://" class="btn">เปิดแอป</a>
+        </div>
+        <div class="my-5">
+          <a href="smartsales://auth-sso" class="btn">Login with SSO</a>
+        </div>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
