730c512acb667b948753b6e612c621fcd474657c
Chong <monkey.d.chong@gmail.com>
2022-01-26 13:01:10 +0700

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,31 +1,57 @@
 <template>
   <div>
-      <div class="inner">
-        <div class="my-5">
-          <a href="smartsales://" class="btn">smartsales://</a>
-        </div>
-        <div class="my-5">
-          <a href="smartsales://auth-sso" class="btn">smartsales://auth-sso</a>
-        </div>
+    <div class="inner">
+      <div class="my-5">
+        <a href="smartsales://" @click.prevent="openApp" class="btn">smartsales://</a>
+      </div>
+      <div class="my-5">
+        <a href="smartsales://auth-sso" @click.prevent="openApp" class="btn">smartsales://auth-sso</a>
+      </div>
 
+      <div class="my-5">
+        <a href="https://smartsales/" @click.prevent="openApp" class="btn">https://smartsales/</a>
+      </div>
 
-        <div class="my-5">
-          <a href="https://smartsales/" class="btn">https://smartsales/</a>
-        </div>
+      <div class="my-5">
+        <a href="https://smartsales/auth-sso" @click.prevent="openApp" class="btn"
+          >https://smartsales/auth-sso</a
+        >
+      </div>
 
-        <div class="my-5">
-          <a href="https://smartsales/auth-sso" class="btn">https://smartsales/auth-sso</a>
-        </div>
-
-        <div class="my-5">
-          <a href="https://smartsales.com" class="btn">https://smartsales.com</a>
-        </div>
+      <div class="my-5">
+        <a href="https://smartsales.com" class="btn" @click.prevent="openApp"
+          >https://smartsales.com</a
+        >
       </div>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
-}
+  name: "IndexPage",
+
+  data() {
+    return {
+      isApple: false,
+      autoOpenUrl: false,
+    };
+  },
+
+  methods: {
+    openApp( evt ) {
+
+      let url = evt.target.href;
+      console.log( url );
+
+      // this.autoOpenUrl = 
+
+      location.replace(url);
+      // if (this.isApple) {
+
+      // }
+    },
+
+  },
+};
 </script>
